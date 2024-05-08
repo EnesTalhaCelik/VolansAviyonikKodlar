4a79ca3caf0c7353c21ae7d67225d567673156dc
Enes Talha Çelik <147333492+EnesTalhaCelik@users.noreply.github.com>
2024-05-08 16:12:17 +0300

    Update Aviyonik.ino

--- Aviyonik.ino
+++ Aviyonik.ino
@@ -2,26 +2,23 @@
 #include "Arduino.h"
 
 
-
-//reciver
-
 #define LED 27
 #define LEDMESAJ 26
 #define AUX_PIN 34
-#define E32TX_PIN 35
-#define E32RX_PIN 32
+#define E32TX_PIN 35 //e32'nin TX PİNİ!!! yani rx
+#define E32RX_PIN 32 //e32'nin RX PİNİ!!! yani rx
 #define M1_PIN 23
 #define M0_PIN 25
 
 
-SoftwareSerial mySerial(35, 32);
+SoftwareSerial loraSerial(E32TX_PIN, E32RX_PIN);
+byte decimalNumber = 0; //kod içerisinde lazım 
 
-byte decimalNumber = 0; // Your decimal number
 void setup() {
-  // put your setup code here, to run once:
+ 
   pinMode(LED,OUTPUT);
   pinMode(LEDMESAJ,OUTPUT);
-  mySerial.begin(115200);
+  loraSerial.begin(115200);
   Serial.begin(9600);
 
 
@@ -33,22 +30,22 @@ void loop() {
   digitalWrite(LED,HIGH);
     delay(5000);
  
-      if (mySerial.available()>1){
-      int x = mySerial.read();
+      if (loraSerial.available()>1){
+      int x = loraSerial.read();
       digitalWrite(LEDMESAJ,HIGH);
       delay(100);
       digitalWrite(LEDMESAJ,LOW);
        }  
 
-  mySerial.write(decimalNumber);
-  mySerial.write(0x2C); 
-  mySerial.write(0x17); 
-  mySerial.write(0x76);//V
-  mySerial.write(0x6F);//O
-  mySerial.write(0x6C);//L
-  mySerial.write(0x61);//A
-  mySerial.write(0x6E);//N
-  mySerial.write(0x73);//S
+  loraSerial.write(decimalNumber);
+  loraSerial.write(0x2C); 
+  loraSerial.write(0x17); 
+  loraSerial.write(0x76);//V
+  loraSerial.write(0x6F);//O
+  loraSerial.write(0x6C);//L
+  loraSerial.write(0x61);//A
+  loraSerial.write(0x6E);//N
+  loraSerial.write(0x73);//S
 
   digitalWrite(LED,LOW);
   delay(5000);
